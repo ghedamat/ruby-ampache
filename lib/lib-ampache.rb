fe502e72818ef4ceb6d3867bd9d7c9a84f840ad9
ghedamat <ghedamat@gmail.com>
2011-09-24 22:47:54 +0200

    trying to fix mplayer issues

--- lib/lib-ampache.rb
+++ lib/lib-ampache.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'open-uri'
 require 'digest/sha2'
 
-require File.join(File.dirname(__FILE__),'lib-ampache')
+require File.join(File.dirname(__FILE__), 'lib-ampache')
 
 =begin
 Class is initialized with Hostname, user and password
@@ -17,74 +17,74 @@ To get albums from an artist you can issue
 artist_instance.albums or ampache_ruby.instance.albums(artist_instance)
 =end
 class AmpacheRuby
-    
-    
-    def initialize(host,user,psw)
-        uri = URI.parse(host)
-        @host = uri.host
-        @path = uri.path
-        @user = user
-        @psw = psw
-        @token = nil
-        @token = getAuthToken(user,psw)
-    end
 
-    attr_accessor :host, :path, :user, :psw, :token, :playlist
-    
-    # tryies to obtain an auth token 
-    def getAuthToken(user,psw)
-        action= "handshake"
-        # auth string
-        key =  Digest::SHA2.new << psw 
-        time = Time.now.to_i.to_s
-        psk = Digest::SHA2.new << (time + key.to_s)
 
-        args = { 'auth' => psk, 'timestamp'=> time, 'version' => '350001', 'user' => user}
-        doc = callApiMethod(action,args);
+  def initialize(host, user, psw)
+    uri = URI.parse(host)
+    @host = uri.host
+    @path = uri.path
+    @user = user
+    @psw = psw
+    @token = nil
+    @token = getAuthToken(user, psw)
+  end
 
-        return doc.at("auth").content
-    end
-    
-    # generic api method call
-    def callApiMethod( method, args={})
-        args['auth'] ||= token if token
-        url = path + "/server/xml.server.php?action=#{method}&#{args.keys.collect { |k| "#{k}=#{args[k]}"}.join('&')}" 
-        response = Net::HTTP.get_response(host,url )
-        return Nokogiri::XML(response.body)
-    end
+  attr_accessor :host, :path, :user, :psw, :token, :playlist
 
-    # retrive artists lists from database,
-    # name is an optional filter
-    def artists(name = nil)
-        args = {}
-        args = { 'filter' => name.to_s } if name  # artist search 
-        artists = []
-        doc = callApiMethod("artists",args)
-        doc.xpath("//artist").each do |a|
-            artists << AmpacheArtist.new(self,a['id'] ,a.at("name").content)
-        end
-        return artists
-    end
+  # tryies to obtain an auth token
+  def getAuthToken(user, psw)
+    action= "handshake"
+    # auth string
+    key = Digest::SHA2.new << psw
+    time = Time.now.to_i.to_s
+    psk = Digest::SHA2.new << (time + key.to_s)
 
-    def albums(artist)
-        albums = []
-        args = { 'filter' => artist.uid.to_s }
-        doc = callApiMethod("artist_albums",args)
-        doc.xpath("//album").each do |a|
-            albums  << AmpacheAlbum.new(self,a['id'], a.at("name").content, artist)
-        end
-        return albums
-    end
+    args = {'auth' => psk, 'timestamp'=> time, 'version' => '350001', 'user' => user}
+    doc = callApiMethod(action, args);
 
-    def songs(album)
-        songs = []
-        args = { 'filter' => album.uid.to_s }
-        doc = callApiMethod("album_songs",args)
-        doc.xpath("//song").each do |s|
-            songs << AmpacheSong.new(self,s['id'], s.at("title").content, album.artist, album, s.at("url").content)
-        end
-        return songs
+    return doc.at("auth").content
+  end
+
+  # generic api method call
+  def callApiMethod(method, args={})
+    args['auth'] ||= token if token
+    url = path + "/server/xml.server.php?action=#{method}&#{args.keys.collect { |k| "#{k}=#{args[k]}" }.join('&')}"
+    response = Net::HTTP.get_response(host, url)
+    return Nokogiri::XML(response.body)
+  end
+
+  # retrive artists lists from database,
+  # name is an optional filter
+  def artists(name = nil)
+    args = {}
+    args = {'filter' => name.to_s} if name # artist search
+    artists = []
+    doc = callApiMethod("artists", args)
+    doc.xpath("//artist").each do |a|
+      artists << AmpacheArtist.new(self, a['id'], a.at("name").content)
     end
-    
+    return artists
+  end
+
+  def albums(artist)
+    albums = []
+    args = {'filter' => artist.uid.to_s}
+    doc = callApiMethod("artist_albums", args)
+    doc.xpath("//album").each do |a|
+      albums << AmpacheAlbum.new(self, a['id'], a.at("name").content, artist)
+    end
+    return albums
+  end
+
+  def songs(album)
+    songs = []
+    args = {'filter' => album.uid.to_s}
+    doc = callApiMethod("album_songs", args)
+    doc.xpath("//song").each do |s|
+      songs << AmpacheSong.new(self, s['id'], s.at("title").content, album.artist, album, s.at("url").content)
+    end
+    return songs
+  end
+
 end
 
--- lib/lib-classes.rb
+++ lib/lib-classes.rb
@@ -1,185 +1,205 @@
 require 'open4'
 require 'timeout'
 class AmpacheArtist
-    
-    # include play module
-    def initialize(ar,uid,name)
-        @ar = ar
-        @uid = uid
-        @name = name
-    end
 
-    attr_reader :uid, :name
+  # include play module
+  def initialize(ar, uid, name)
+    @ar = ar
+    @uid = uid
+    @name = name
+  end
 
-    def albums
-        @albums ||= @ar.albums(self)
-    end
+  attr_reader :uid, :name
+
+  def albums
+    @albums ||= @ar.albums(self)
+  end
+
+  def add_to_playlist(pl)
+  albums.each do |s|
+    s.add_to_playlist(pl)
+    sleep 1
+  end
+end
 end
 
 
 class AmpacheAlbum
-    
-    def initialize(ar,uid,name,artist)
-        @ar = ar
-        @uid = uid
-        @name = name
-        @artist = artist
-    end
 
-    attr_reader :uid, :name, :artist
+  def initialize(ar, uid, name, artist)
+    @ar = ar
+    @uid = uid
+    @name = name
+    @artist = artist
+  end
 
-    def songs
-        @songs ||= @ar.songs(self)
-    end
+  attr_reader :uid, :name, :artist
 
-    def addToPlaylist(pl)
-        songs.each do |s|
-            s.addToPlaylist(pl)
-        end
+  def songs
+    @songs ||= @ar.songs(self)
+  end
+
+  def add_to_playlist(pl)
+    songs.each do |s|
+      s.add_to_playlist(pl)
+      sleep 1
     end
+  end
 
 end
 
 class AmpacheSong
 
-    def initialize(ar,uid,title,artist,album, url) 
-        @ar = ar
-        @uid = uid
-        @title = title
-        @artist = artist
-        @album = album
-        @url = url
-    end
+  def initialize(ar, uid, title, artist, album, url)
+    @ar = ar
+    @uid = uid
+    @title = title
+    @artist = artist
+    @album = album
+    @url = url
+  end
 
-    attr_reader :uid, :title, :artist, :album, :url
+  attr_reader :uid, :title, :artist, :album, :url
 
-    def addToPlaylist(pl)
-        pl.add(self)
-    end
+  def add_to_playlist(pl)
+    pl.add(self)
+  end
 
 end
 
-class AmpachePlaylist 
-    def add(song)
-    puts song.title
-        if !@pid
-            $options[:path] ||= '/usr/bin/mplayer'
-            $options[:timeout] ||= 15
-            mplayer_options = "-slave -quiet"
-            mplayer = "#{$options[:path]} #{mplayer_options} \"#{song.url}\""
-            @pid,@stdin,@stdout,@stderr = Open4.popen4(mplayer)
-            begin
-                t = Timeout::timeout($options[:timeout]) do 
-                    until @stdout.gets.inspect =~ /playback/ do
-                    # puts @stdout.gets.inspect
+class AmpachePlaylist
 
-                    end
+  def initialize
+    mplayer_start
+    @started = false
+  end
 
-                end
-            rescue Timeout::Error  
-                puts "err on reading data"
-            end
-        else
-            begin
-                @stdin.puts "loadfile \"#{song.url}\" 1"
-            rescue Errno::EPIPE
-                puts "error on adding song to playlist" 
-                @pid = nil
-                add(song)
-             end
+  def started?
+    @started == true
+  end
+
+  def started!
+    @started = true
+  end
+
+  def mplayer_start
+    $options[:path] ||= '/usr/bin/mplayer'
+    $options[:timeout] ||= 15
+    mplayer_options = "-slave -quiet -idle"
+    mplayer = "#{$options[:path]} #{mplayer_options} "
+    @pid, @stdin, @stdout, @stderr = Open4.popen4(mplayer)
+  end
+
+  def console
+    while ((cmd = gets.chomp)  != 'exit' )
+      @stdin.puts cmd
+    end
+  end
+
+  def add(song)
+
+    say(HighLine.new.color("adding song ",:green) + "#{song.title}")
+    if !@pid
+      started!
+      mplayer_start
+    end
+    begin
+      started!
+      @stdin.puts "loadfile \"#{song.url}\" 1"
+    rescue Errno::EPIPE
+      puts "error on adding song to playlist"
+      @pid = nil
+      @started = false
+      add(song)
+    end
+
+  end
+
+  def stop
+    begin
+      @stdin.puts "quit" if @pid
+    rescue Errno::EPIPE
+      puts "playlist is over"
+    end
+    @pid = nil
+    @started = false
+  end
+
+  def pause
+    begin
+      @stdin.puts "pause" if @pid && started?
+    rescue Errno::EPIPE
+      puts "playlist is over"
+    end
+  end
+
+  def next
+    begin
+      @stdin.puts "pt_step 1 1" unless @pid.nil?
+    rescue Errno::EPIPE => e
+      puts "playlist is over on next"
+      @pid = nil
+    end
+  end
+
+  def now_playing
+    return "Not playing man!" unless started?  && !@pid.nil?
+    begin
+      s = get("meta_title")
+      return "data not available OR playlist is over"  if s.nil?
+      s+= get("meta_artist") rescue s
+      s+= get("meta_album")  rescue s
+      s.chomp!
+      return s
+
+    rescue Errno::EPIPE => e
+      @pid = nil
+      @started = false
+      return "playlist is over here" #, or you got an ERROR from mplayer: #{e.to_s}"
+
+    end
+  end
+
+  # I borrowed these two methods from the author of mplayer-ruby!
+  # so my thanks to Artuh Chiu and his great gem mplayer-ruby
+  def get(value)
+    field = value.to_s
+    match = case field
+    when "time_pos" then
+      "ANS_TIME_POSITION"
+    when "time_length" then
+      "ANS_LENGTH"
+    when "file_name" then
+      "ANS_FILENAME"
+    else
+      "ANS_#{field.upcase}"
+    end
+    res = command("get_#{value}", /#{match}/)
+    res.gsub("#{match}=", "").gsub("'", "")  unless res.nil?
+  end
+
+  def command(cmd, match = //)
+    @stdin.puts(cmd)
+    response = ""
+    begin
+      t = Timeout::timeout(3) do
+        until response =~ match
+          response = @stdout.gets
+    #XXX escaping bad utf8 chars
+          ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+          if response
+            response =ic.iconv(response + ' ')[0..-2]
+          else
+            response == ''
+          end
         end
-    end
-
-    def play
-        @player.play if @pid 
-    end
-
-    def stop
-        begin
-            @stdin.puts "quit" if @pid
-        rescue Errno::EPIPE
-            puts "playlist is over" 
-        end
-        @pid = nil
-    end
-
-    def pause
-        begin
-            @stdin.puts "pause" if @pid
-        rescue Errno::EPIPE
-            puts "playlist is over" 
-        end
-    end
-
-    def next
-        begin
-            @stdin.puts "pt_step 1 1" if @pid
-        rescue Errno::EPIPE => e
-            puts "playlist is over on next" 
-            @pid = nil
-        end
-    end
-    
-    def nowPlaying
-        return "Not playing man!" unless @pid
-        begin
-            s= ''
-            @tim = 0
-            begin
-                t = Timeout::timeout(3) do 
-                    #s+= get("file_name") 
-                    s+= get("meta_title")  
-                    s+= get("meta_artist")  
-                    s+= get("meta_album")  
-                    s.chomp!
-                end
-                @tim = 0
-                return s
-            rescue Timeout::Error
-                @tim += 1
-                if (@tim > 3 )
-                    @tim = 0
-                    return "err" 
-                else
-                    return nowPlaying 
-                end
-            end
-        rescue Errno::EPIPE => e
-            @pid = nil
-            return "playlist is over here" #, or you got an ERROR from mplayer: #{e.to_s}" 
-
-        end
-    end
-    
-    # I borrowed these two methods from the author of mplayer-ruby!
-    # so my thanks to Artuh Chiu and his great gem mplayer-ruby
-    def get(value)
-      field = value.to_s
-      match = case field
-      when "time_pos" then "ANS_TIME_POSITION"
-      when "time_length" then "ANS_LENGTH"
-      when "file_name" then "ANS_FILENAME"
-      else "ANS_#{field.upcase}"
       end
-      res = command("get_#{value}",/#{match}/)
-      res.gsub("#{match}=","").gsub("'","")
+      return response.gsub("\e[A\r\e[K", "")
+    rescue Timeout::Error
+      return
     end
 
-    def command(cmd,match = //)
-      @stdin.puts(cmd)
-      response = ""
-      until response =~ match
-        response = @stdout.gets
-#XXX escaping bad utf8 chars
-        ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-        if response
-          response =ic.iconv(response + ' ')[0..-2] 
-        else
-          response == ''
-        end
-      end
-      response.gsub("\e[A\r\e[K","")
-    end
+  end
 
 end
 
